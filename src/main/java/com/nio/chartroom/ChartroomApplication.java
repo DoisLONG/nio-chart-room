6e07d7405dfda68561b724fe32756ed8f2b6c347
DoisLONG <15221580643@163.com>
2020-10-20 17:07:47 +0800

    项目初始化

---
+++ src/main/java/com/nio/chartroom/ChartroomApplication.java
@@ -0,0 +1,13 @@
+package com.nio.chartroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChartroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nio/chartroom/ChartroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nio.chartroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChartroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
