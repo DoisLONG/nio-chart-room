16bb60decd9d10a0960aadb019c25088ee24f392
DoisLONG <15221580643@163.com>
2020-10-21 10:43:39 +0800

    Nio client & Nio Server

---
+++ src/main/java/com/nio/chartroom/NioClient.java
@@ -0,0 +1,42 @@
+package com.nio.chartroom;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
+/**
+ *  NIO 客户端
+ * */
+public class NioClient {
+    /**
+     *  启动
+     * */
+    private  void start() throws IOException {
+        /**
+         *  连接服务器端
+         * */
+        SocketChannel socketChannel = SocketChannel.open(new InetSocketAddress("127.0.0.1", 8000));
+        /**
+         *  向服务器端发送数据
+         * */
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()){
+            String request = scanner.nextLine();
+            if (request != null && request.length() > 0 ) {
+                socketChannel.write(Charset.forName("UTF-8").encode(request));
+            }
+        }
+
+        /**
+         *  接收服务器端响应
+         * */
+        // 新开线程，专门负责来接收服务器端的响应数据
+        // selector , socketChannel
+    }
+
+    public static void main(String[] args) throws IOException {
+        new NioClient().start();
+    }
+}
